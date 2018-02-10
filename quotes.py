1f77b6ac036c5f377de5da6c62a162c8119e3f6a
Ian Nanez <inaneze@gmail.com>
2018-02-10 18:23:36 -0500

    zecond commit

--- quotes.py
+++ quotes.py
@@ -1,10 +1,10 @@
 def send_simple_message():
     return requests.post(
-        "https://api.mailgun.net/v3/sandbox861647c9ee8d474e950bb188c549808b.mailgun.org/messages",
-        auth=("api", "key-4df658831a75f41a44941ed2d7aa39f9"),
-        data={"from": "Mailgun Sandbox <postmaster@sandbox861647c9ee8d474e950bb188c549808b.mailgun.org>",
-              "to": "Ian Nanez <inaneze@gmail.com>",
-              "subject": "Hello Ian Nanez",
-              "text": "Congratulations Ian Nanez, you just sent an email with Mailgun!  You are truly awesome!"})
+        "hehehe",
+        auth=("api", "hehehe"),
+        data={"from": "Mailgun Sandbox <hehehe>",
+              "to": "hehehe <hehehe>",
+              "subject": "Hello heheh",
+              "text": "Congratulations hehehe, you just sent an email with Mailgun!  You are truly awesome!"})
 
 send_simple_message
