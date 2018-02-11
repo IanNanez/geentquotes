d38c9895e0eb64cd3d6b8e691d2776a27bae0d77
Ian Nanez <inaneze@gmail.com>
2018-02-11 14:23:12 -0500

    zecond commit

--- quotes.py
+++ quotes.py
@@ -1,10 +1,20 @@
-def send_simple_message():
-    return requests.post(
-        "hehehe",
-        auth=("api", "hehehe"),
-        data={"from": "Mailgun Sandbox <hehehe>",
-              "to": "hehehe <hehehe>",
-              "subject": "Hello heheh",
-              "text": "Congratulations hehehe, you just sent an email with Mailgun!  You are truly awesome!"})
+def send_complex_message(recipient):
+    http = httplib2.Http()
+    http.add_credentials('api', MAILGUN_API_KEY)
 
-send_simple_message
+    url = 'https://api.mailgun.net/v3/{}/messages'.format(MAILGUN_DOMAIN_NAME)
+    data = {
+        'from': 'Example Sender <mailgun@{}>'.format(MAILGUN_DOMAIN_NAME),
+        'to': recipient,
+        'subject': 'This is an example email from Mailgun',
+        'text': 'Test message from Mailgun',
+        'html': '<html>HTML <strong>version</strong> of the body</html>'
+    }
+
+    resp, content = http.request(
+        url, 'POST', urlencode(data),
+        headers={"Content-Type": "application/x-www-form-urlencoded"})
+
+    if resp.status != 200:
+        raise RuntimeError(
+            'Mailgun API error: {} {}'.format(resp.status, content))
